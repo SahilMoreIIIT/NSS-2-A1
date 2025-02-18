d25b197f5dcdde84eee74f0bef96bd9abbb65cee
Sahil Vinod More <142232037+SahilMoreIIIT@users.noreply.github.com>
2025-02-18 18:52:58 +0530

    .

--- getacl.c
+++ getacl.c
@@ -1,36 +1,32 @@
+#include <sys/types.h>
+#include <sys/extattr.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/xattr.h>
 
 #define BUFFER_SIZE 1024
 
-// Function to load ACL from the extended attributes of the file
-int load_acl(const char *file, char *acl_str) {
-    ssize_t acl_attr = getxattr(file, "user.acl", acl_str, BUFFER_SIZE);
-    if (acl_attr == -1) {
-        perror("Error: No ACL found for the file");
-        return 0;
-    }
-    return 1;
-}
-
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Usage: %s <file>\n", argv[0]);
+        fprintf(stderr, "Usage: getacl <file>\n");
         return 1;
     }
 
     const char *file = argv[1];
     char acl_str[BUFFER_SIZE];
+    ssize_t acl_len;
 
-    if (!load_acl(file, acl_str)) {
+    // Retrieve extended attribute (user.acl) from the file
+    acl_len = extattr_get_file(file, EXTATTR_NAMESPACE_USER, "acl", acl_str, sizeof(acl_str));
+    if (acl_len == -1) {
+        perror("Error getting ACL");
         return 1;
     }
 
+    // Null-terminate the retrieved ACL string
+    acl_str[acl_len] = '\0';
+
     // Print the retrieved ACL
-    printf("ACL for file '%s':\n%s\n", file, acl_str);
+    printf("ACL for file '%s': %s\n", file, acl_str);
     return 0;
 }
--- setacl.c
+++ setacl.c
@@ -1,52 +1,31 @@
+#include <sys/types.h>
+#include <sys/extattr.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/xattr.h>
 
 #define BUFFER_SIZE 1024
 
-// Function to check if a user exists
-int user_exists(const char *username) {
-    struct passwd *pw = getpwnam(username);
-    return pw != NULL;
-}
-
-// Function to set ACL for the file by writing to extended attributes
-int set_acl(const char *file, const char *acl_str) {
-    if (setxattr(file, "user.acl", acl_str, strlen(acl_str), 0) == -1) {
-        perror("Error: Could not set ACL for file");
-        return 0;
-    }
-    return 1;
-}
-
 int main(int argc, char **argv) {
     if (argc < 4) {
-        printf("Usage: %s <perms> <user> <file>\n", argv[0]);
+        fprintf(stderr, "Usage: setacl <perms> <user> <file>\n");
         return 1;
     }
 
     const char *perms = argv[1];
     const char *user = argv[2];
     const char *file = argv[3];
-
-    // Validate user
-    if (!user_exists(user)) {
-        printf("Error: User '%s' does not exist\n", user);
-        return 1;
-    }
-
-    // Create ACL string in the form of "user:user_name,perms"
     char acl_str[BUFFER_SIZE];
-    snprintf(acl_str, BUFFER_SIZE, "user:%s,%s", user, perms);
 
-    // Set ACL for the file
-    if (!set_acl(file, acl_str)) {
+    // Prepare the ACL string to be set (e.g., user:username,rw-)
+    snprintf(acl_str, sizeof(acl_str), "user:%s,%s", user, perms);
+
+    // Set extended attribute (user.acl) to the file
+    if (extattr_set_file(file, EXTATTR_NAMESPACE_USER, "acl", acl_str, strlen(acl_str)) == -1) {
+        perror("Error setting ACL");
         return 1;
     }
 
-    printf("Successfully set ACL for user '%s' on file '%s' with permissions '%s'\n", user, file, perms);
+    printf("Successfully set ACL for file '%s'\n", file);
     return 0;
 }
